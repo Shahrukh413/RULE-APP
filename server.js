d192640b6d964305541f323324e609374ee96072
Shakruk <mr.srk8058@gmail.com>
2024-10-20 20:12:01 +0530

    initial

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const ruleRoutes = require('./routes/ruleRoutes');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// mongoose.connect('mongodb+srv://imsantoshsahoo1430:0001432@cluster0.2msp6vq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+
+mongoose.connect('mongodb+srv://mrsrk4444:c4tGQAUdzvS2Ti0C@cluster0.wx0uw.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch(err => {
+  console.error("Error connecting to MongoDB", err);
+});
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for serving the index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/index.html'));
+});
+app.use('/api/rules', ruleRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
